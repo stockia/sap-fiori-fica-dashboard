4fba030bd90e8b7bfe358d36747bee8d41d683c7
Alex Stocki <ladislao.stocki@gointegro.com>
2026-01-27 11:47:33 -0300

    feat: add client-side search functionality

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("com.stocki.ficadashboard.controller.Main", {
@@ -31,6 +35,37 @@ sap.ui.define([
                 ])
                 
                 MessageToast.show(sMessage);
+            },
+
+            /**
+             * Function to filter the list based on search input
+             * Filters by name or contractAccount
+             */
+            onSearch: function (oEvent) {
+                // get the search query
+                var sQuery = oEvent.getParameter("query");
+                var aFilters = [];
+
+                if (sQuery && sQuery.length > 0) {
+                    // create filters for name and contractaccount
+                    var oFilterName = new Filter("name", FilterOperator.Contains, sQuery);
+                    var oFilterContractAccount = new Filter("contractAccount", FilterOperator.Contains, sQuery);
+
+                    // combine filters with OR logic
+                    var oCombinedFilter = new Filter({
+                        filters: [oFilterName, oFilterContractAccount],
+                        and: false
+                    });
+
+                    aFilters.push(oCombinedFilter);
+                }
+
+                // get the table and its binding
+                var oTable = this.byId("idProductsTable");
+                var oBinding = oTable.getBinding("items");
+
+                // apply the filters to the binding
+                oBinding.filter(aFilters);
             }
         });
     });
